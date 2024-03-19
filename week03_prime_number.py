e7e36ace2aff2f825b389c54f565431ef6a211b7
umjunsik <hwangjs070821@gmail.com>
2024-03-19 20:57:03 +0900

    v1.1 prime number

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,16 +1,22 @@
-start, end = map(int, input("Input number : ").split())
+def is_prime_number(n) -> bool:
+        """
+        prime number determination function
+        :param n: a positive integer
+        :return: Returns True if it is a prime number, and returns FLase if it is not a prime number.
+        """
+        if k < 2:
+               return False
+        else:
+                i = 2
+                while i * i <= k:
+                        if k % i == 0:
+                                return False
+                        i = i + 1
+        return True
+
+start, end = map(int, input("Input start and end number : ").split())
 
 for k in range(start, end+1):
-    is_prime_number = True
-    if k < 2:
-         is_prime_number = False
-    else:
-        i = 2
-        while i*i <=k:
-            if k % i == 0:
-                is_prime_number = False
-                break
-            i = i + 1
-        if is_prime_number: print(k, end=' ')
+        if is_prime_number(k): print(k, end=' ')
 
 
